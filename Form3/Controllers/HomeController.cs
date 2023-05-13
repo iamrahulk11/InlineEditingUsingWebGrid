e7e27059cdf1a99c0c33c25300cc09f0df9b88cd
Rahul Kumar <79801219+iamrahulk11@users.noreply.github.com>
2023-05-13 11:50:07 +0530

    Add project files.

---
+++ Form3/Controllers/HomeController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Form3.Models;
+
+namespace Form3.Controllers
+{
+    public class HomeController : Controller
+    {
+        form2dbEntities db = new form2dbEntities();
+        public ActionResult Index()
+        {
+           /* List<userdb> list = new List<userdb>();
+            var data = db.userdbs.ToList();
+            foreach (var item in data)
+            {
+                userdb model = new userdb();
+                model.userid = item.userid;
+                model.User_Name = item.User_Name;
+                model.Email = item.Email;
+                model.Contact = item.Contact;
+                model.Gender = item.Gender;
+                model.Date_Of_Birth = item.Date_Of_Birth;
+                model.Address = item.Address;
+                model.Country = item.Country;
+                model.State = item.State;
+                model.Hobbies = item.Hobbies;
+               // Session["img"] = item.ImagePath;
+                model.ImagePath = item.ImagePath;
+                list.Add(model);
+            }
+
+            return View(list);*/
+            return View(db.userdbs.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Update(userdb model)
+        {
+            List<userdb> list = new List<userdb>();
+            if (model.FileName != null)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(model.FileName.FileName);
+                string extension = Path.GetExtension(model.FileName.FileName);
+                HttpPostedFileBase postedFile = model.FileName;
+                string FileLength = Convert.ToString(postedFile.ContentLength);
+
+                fileName += extension;
+                model.ImagePath = "~/images/" + fileName;
+                fileName = Path.Combine(Server.MapPath("~/images/"), fileName);
+
+                model.FileName.SaveAs(fileName);
+                userdb user = db.userdbs.Where(u => u.userid == model.userid).FirstOrDefault();
+                user.User_Name = model.User_Name;
+                user.Email = model.Email;
+                user.Contact = model.Contact;
+                user.Gender = model.Gender;
+                user.Date_Of_Birth = model.Date_Of_Birth;
+                user.Address = model.Address;
+                user.Country = model.Country;
+                user.State = model.State;
+                user.Hobbies = model.Hobbies;
+                user.ImagePath = model.ImagePath;
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                list.Add(user);
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                userdb user = db.userdbs.Where(em => em.userid == model.userid).FirstOrDefault();
+                 user.User_Name = model.User_Name;
+                 user.Email = model.Email;
+                 user.Contact = model.Contact;
+                 user.Gender = model.Gender;
+                 user.Date_Of_Birth = model.Date_Of_Birth;
+                 user.Address = model.Address;
+                 user.Country = model.Country;
+                 user.State = model.State;
+                 user.Hobbies = model.Hobbies;
+                
+                
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                list.Add(user);
+                return RedirectToAction("Index"); 
+            }
+            
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+
+            return View();
+        }
+    }
+}
---
+++ Form3/Scripts/MyCutsom/MyCustom.js
@@ -0,0 +1,140 @@
+﻿$(document).ready(function () {
+
+    $('.edit-mode').hide();
+    $('.edit-user, .cancel-user').on('click', function () {
+        var tr = $(this).parents('tr:first');
+        tr.find('.edit-mode, .display-mode').toggle();
+    });
+    
+
+    $('.edit-user').on('click', function () {
+        debugger;
+        var tr = $(this).parents('tr:first');
+        var userid = tr.find("#userid").html();
+        var User_Name = tr.find("#NameText").val();
+        var Email = tr.find("#EmailText").val();
+        var Contact = tr.find("#ContactText").val();
+
+        var Gender = "";
+        var selectedGender = document.getElementsByName('Gender');
+        
+        for (i = 0; i < selectedGender.length; i++) {
+            if (selectedGender[i].value == tr.find("#lblGender").text()) {
+                 Gender= selectedGender[i].value ;
+                selectedGender[i].checked = true;
+            }
+        }
+        
+        var Address = tr.find("#AddressText").val();
+        var DateOfBirth = tr.find("#DateOfBirthText").val();
+        var Country = tr.find("#CountryText").val();
+        var State = tr.find("#StateText").val();
+        var Hobbies = tr.find("#HobbiesText").val();
+       // var ImagePath = tr.find("#ImagePath").val();
+
+        tr.find("#lblName").text(User_Name);
+        tr.find("#lblEmail").text(Email);
+        tr.find("#lblContact").text(Contact);
+        tr.find("#lblGender").text(Gender);
+       
+        
+        tr.find("#lblDob").text(DateOfBirth);
+        tr.find("#lblAddress").text(Address);
+        tr.find("#lblCountry").text(Country);
+        tr.find("#lblState").text(State);
+        tr.find("#lblHobbies").text(Hobbies);
+       // tr.find("#lblImage").text(ImagePath);
+        
+
+    });
+
+    $('.save-user').on('click', function () {
+        debugger;
+        var tr = $(this).parents('tr:first');
+
+        var userid = tr.find("#userid").html();
+
+        var User_Name = tr.find("#NameText").val();
+        var Email = tr.find("#EmailText").val();
+        var Contact = tr.find("#ContactText").val();
+
+        var selectedGender = document.getElementsByName('Gender');
+        var Gender = "";
+        for (i = 0; i < selectedGender.length; i++) {
+            if (selectedGender[i].checked) {
+                Gender = selectedGender[i].value;
+            }
+        }
+
+        //var Gender = tr.find("#GenderText").val();
+        var DateOfBirth = tr.find("#DateOfBirthText").val();
+        var Address = tr.find("#AddressText").val();
+        var Country = tr.find("#CountryText").val();
+        var State = tr.find("#StateText").val();
+        var Hobbies = tr.find("#HobbiesText").val();
+
+
+
+        var ImagePath = "";
+        if ($("#ImageText").val() != "") {
+            var extension = tr.find("#ImageText").val().split('.').pop().toUpperCase();
+            if (extension != "PNG" && extension != "JPG" && extension != "JPEG") {
+
+                alert('Imvalid image file format,File format should be jpg, png, jpeg');
+                return false;
+            }
+            ImagePath = tr.find('#ImageText').get(0).files;
+        }
+        var file = ImagePath[0];
+        var UserModel = "";
+
+        if ($("#ImageText").val() != "") {
+            UserModel =
+            {
+                "userid": userid,
+                "User_Name": User_Name,
+                "Email": Email,
+                "Contact": Contact,
+                "Gender": Gender,
+                "Date_Of_Birth": DateOfBirth,
+                "Address": Address,
+                "Country": Country,
+                "State": State,
+                "Hobbies": Hobbies,
+                "FileName": file
+            };
+        } else {
+            UserModel =
+            {
+                "userid": userid,
+                "User_Name": User_Name,
+                "Email": Email,
+                "Contact": Contact,
+                "Gender": Gender,
+                "Date_Of_Birth": DateOfBirth,
+                "Address": Address,
+                "Country": Country,
+                "State": State,
+                "Hobbies": Hobbies,
+
+            };
+        }
+
+        $.ajax({
+            url: '/Home/Update',
+            data: JSON.stringify(UserModel),
+            type: 'POST',
+            contentType: 'application/json; charset=utf-8',
+            success: function (data) {
+                tr.find('.edit-mode, .display-mode').toggle();
+                alert('Record updated Successfully!!');
+            },
+            error: function () {
+                alert('Failed to save');
+            }
+        });
+
+    });
+});
+
+
